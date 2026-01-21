3aabe0314e1d8bb893d041cc924eaa1e31dc2501
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-20 19:54:40 -0800

    File Created ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ReverseString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number of string:");
+        int n=sc.nextInt();
+
+        String[] str=new String[n];
+        System.out.println("Enter Strings:");
+
+        for(int i=0;i<n;i++){
+            str[i]=sc.next();
+        }
+
+        System.out.println("String Reversed:");
+        for(int i=0;i=n-1;i>=0;i--){
+            System.out.println(str[i]);
+        }
+    }
+}
