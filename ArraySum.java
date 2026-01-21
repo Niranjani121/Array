c4c5433b561a735ae795edefa0cf66a633550471
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-20 18:42:36 -0800

    File created ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class ArraySum{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter array size:");
         int n=sc.nextInt();
         int[] arr=new int[n];
         int sum=0;
 
+        System.out.println("Enter array elements:");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
             sum+=arr[i];
